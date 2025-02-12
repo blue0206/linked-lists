b6b2958a9aff783af33b1d7cf4ba098c7504d48b
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-13 00:27:20 +0530

    Add console logs to display linked list size and head/tail values after operations

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ list.append("turtle");
 list.prepend("lion");
 list.insertAt("tiger", 4);
 
+console.log(list.size);                 // 8
+console.log(list.head.value);           // lion
+console.log(list.tail.value);           // turtle
 console.log(list.toString());
 
 list.removeAt(5);                       // removes 'hamster'
 list.pop();                             // removes 'turtle'
 
 console.log(list.toString());
+console.log(list.size);                 // 6
+console.log(list.head.value);           // lion
+console.log(list.tail.value);           // snake
 
 console.log(list.find("tiger"));        // 4
 console.log(list.contains("whale"));    // false
